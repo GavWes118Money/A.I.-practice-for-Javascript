88e055824b42b0d4d003f847386e7efb7feb2c87
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-09-15 15:12:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const images = ["https://i.postimg.cc/nLL9pdxV/Cute-cartoon-cat-hand-drawn-on-transparent-background-PNG.png", "https://i.postimg.cc/ncQMDHt7/578-5786255-cute-dog-clipart-png-download.png", "https://i.postimg.cc/xdbcKnm7/Vicky-common-image-28.webp"];
+let currentImage = 0;
 
+setInterval (() => {
+  currentImage++;
+
+  if (currentImage >= images.length) {
+    currentImage = 0;
+  }
+
+  document.getElementById ("slideshow").src = image[currentImage];
+}, 2000);
